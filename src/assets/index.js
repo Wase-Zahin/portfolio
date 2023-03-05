12848fe0c88370c5022daef93f5ead0193f8c40b
Wase-Zahin <w.zahin@yahoo.com>
2023-03-05 23:24:53 +0800

    project section layout fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,5 +22,6 @@ import wechat_icon from "./wechat_icon.png";
 import linkedin_icon from "./linkedin_icon.svg";
 import my_cv from "./CV.pdf";
 import logo from "./logo.png";
+import arrow from "./arrow_top_right.svg";
 
-export {js_icon, mongodb_icon, npm_icon, python_icon, git_icon, github_icon, express_icon, nodejs_icon, css_icon, django_icon, c_plus_plus_icon, c_icon, vuejs_icon, react_icon, moon_icon, cv_maker_ss, kaixinwang_screenshot, e_commerce_screenshot, blog_site_ss, wechat_qr, wechat_icon, linkedin_icon, my_cv, logo}
+export {js_icon, mongodb_icon, npm_icon, python_icon, git_icon, github_icon, express_icon, nodejs_icon, css_icon, django_icon, c_plus_plus_icon, c_icon, vuejs_icon, react_icon, moon_icon, cv_maker_ss, kaixinwang_screenshot, e_commerce_screenshot, blog_site_ss, wechat_qr, wechat_icon, linkedin_icon, my_cv, logo, arrow}
--- src/components/layout/Projects/Projects.js
+++ src/components/layout/Projects/Projects.js
@@ -1,16 +1,16 @@
-import { blog_site_ss, cv_maker_ss, e_commerce_screenshot, kaixinwang_screenshot } from '../../../assets';
+import { arrow, blog_site_ss, cv_maker_ss, e_commerce_screenshot, kaixinwang_screenshot } from '../../../assets';
 import './Projects.css';
 
 const Projects = () => {
     return (
-        <div  id='projects' className="section_padding section_container">
+        <div id='projects' className="section_padding section_container">
             <div className="header_contaner section_margin">
                 <div className="underline"></div>
                 <h1 className="section_name">Projects</h1>
                 <div className="underline"></div>
             </div>
 
-            <div>
+            <div className='projectsWrapper'>
                 <div className='projectWrapper'>
                     <div className='projectPreview'>
                         <img src={kaixinwang_screenshot} alt="Chinese website" className='projectPreview'></img>
@@ -20,7 +20,21 @@ const Projects = () => {
                         <h1>Kaixinwang</h1>
                         <div className='projectInfo'>This is a Chinese website created using only HTML and CSS, featuring gaming content and advertising. The project provided the opportunity to develop skills in design, advertising, and project management, and may have also imparted knowledge about website hosting and content curation. Despite its simplicity, the website serves as a valuable learning experience for those interested in web development and content creation.</div>
                         <hr></hr>
-                        <p className='skillsUsed'>Built using HTML and CSS</p>
+                        <div>
+                            <div>
+                                <p className='skillsUsed'>Built using HTML and CSS</p>
+                                <div className='projectLinks'>
+                                    <div>
+                                        <a href='#'>Live Site</a>
+                                        <img className='arrow_top_right' src={arrow} alt="Top right arrow"></img>
+                                    </div>
+                                    <div>
+                                        <a href='#'>Github</a>
+                                        <img className='arrow_top_right' src={arrow} alt="Top right arrow"></img>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -33,7 +47,19 @@ const Projects = () => {
                         <h1>E-Commerce</h1>
                         <div className='projectInfo'>My e-commerce website has user authentication and a shop page that displays products from an API. Users can view and add items to their cart, which calculates the total price.<br></br>With user login, API integration, and cart functionality, my e-commerce site provides a seamless shopping experience for customers.</div>
                         <hr></hr>
-                        <p className='skillsUsed'>Built using React, Django and CSS.</p>
+                        <div>
+                            <p className='skillsUsed'>Built using React, Django and CSS.</p>
+                            <div className='projectLinks'>
+                                <div>
+                                    <a href='#'>Live Site</a>
+                                    <img className='arrow_top_right' src={arrow} alt="Top right arrow"></img>
+                                </div>
+                                <div>
+                                    <a href='#'>Github</a>
+                                    <img className='arrow_top_right' src={arrow} alt="Top right arrow"></img>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -46,7 +72,19 @@ const Projects = () => {
                         <h1>CV Maker</h1>
                         <div className='projectInfo'>The project is a React-based tool that allows users to create professional resumes by inputting their information. The tool then generates a visually pleasing CV, which can be exported as a PDF.<br></br>Overall, the project provides a convenient and efficient way for users to create professional resumes with minimal effort.</div>
                         <hr></hr>
-                        <p className='skillsUsed'>Built using React class components, CSS and react-to-print.</p>
+                        <div>
+                            <p className='skillsUsed'>Built using React class components, CSS and react-to-print.</p>
+                            <div className='projectLinks'>
+                                <div>
+                                    <a href='#'>Live Site</a>
+                                    <img className='arrow_top_right' src={arrow} alt="Top right arrow"></img>
+                                </div>
+                                <div>
+                                    <a href='#'>Github</a>
+                                    <img className='arrow_top_right' src={arrow} alt="Top right arrow"></img>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -60,8 +98,21 @@ const Projects = () => {
                         <div className='projectInfo'>The blog site offers user login, allowing users to add blogs to their own profile and read individual blogs. Users can create their own content and share it with others.<br></br>The site provides a platform for bloggers to connect with their audience and share their ideas and experiences.</div>
                         <hr></hr>
                         <p className='skillsUsed'>Built using VueJs, Flask and CSS </p>
+                        <div>
+                            <div className='projectLinks'>
+                                <div>
+                                    <a href='#'>Live Site</a>
+                                    <img className='arrow_top_right' src={arrow} alt="Top right arrow"></img>
+                                </div>
+                                <div>
+                                    <a href='#'>Github</a>
+                                    <img className='arrow_top_right' src={arrow} alt="Top right arrow"></img>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     )
